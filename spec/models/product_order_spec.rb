7b2caf34ab585b2de2d37794ce3fb4b20432ee4e
glckfndr <obulan65@gmail.com>
2023-06-08 12:09:00 +0300

    Replace expect on should

--- spec/models/product_order_spec.rb
+++ spec/models/product_order_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ProductOrder, type: :model do
   describe "validation of greater than zero" do
-    it { is_expected.to validate_numericality_of(:amount).is_greater_than(0) }
+    it { should validate_numericality_of(:amount).is_greater_than(0) }
   end
 
   describe 'validation of associations' do
-      it { is_expected.to belong_to(:order)}
-      it { is_expected.to belong_to(:product)}
-    end
+    it { should belong_to(:order)}
+    it { should belong_to(:product)}
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe "validation of precense" do
-    it { is_expected.to validate_presence_of(:name) }
+    it { should validate_presence_of :name}
+
   end
 
   describe "validations of greater than zero" do
-    it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
-    it { is_expected.to validate_numericality_of(:balance).is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0)}
+    it { should validate_numericality_of(:balance).is_greater_than_or_equal_to(0)}
   end
 
   describe 'validations of associations to product_orders' do
-    it { is_expected.to have_many(:orders).through(:product_orders) }
-    it { is_expected.to have_many(:product_orders).dependent(:destroy) }
+    it { should have_many(:orders).through(:product_orders) }
+    it { should have_many(:product_orders).dependent(:destroy) }
   end
 end
