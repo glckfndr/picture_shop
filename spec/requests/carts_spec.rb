e74bebbdb04c5529d6c2ea0d2ef45f0912e71861
glckfndr <obulan65@gmail.com>
2023-06-26 22:23:30 +0300

    Format Carts test

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Carts", type: :request do
+RSpec.describe 'Carts', type: :request do
   let!(:product) { create(:product) }
   let!(:product2) { create(:product) }
 
-  describe "POST add(plus) and sum" do
+  describe 'POST add(plus) and sum' do
     context 'adding products to session:' do
       it 'add one product to session' do
         post add_product_path(product)
         expect(response).to have_http_status(:redirect)
-        expect(session[:cart]["#{product.id}"]).to eq({'amount'=> 1, 'balance' => product.balance})
+        expect(session[:cart]["#{product.id}"]).to eq({ 'amount' => 1, 'balance' => product.balance })
       end
 
       it 'add the same product two times and get sum' do
@@ -17,14 +17,14 @@ RSpec.describe "Carts", type: :request do
         expect(response).to have_http_status(:redirect)
         post plus_product_to_carts_path(id: product.id)
         expect(response).to have_http_status(:redirect)
-        expect(session[:cart]["#{product.id}"]).to eq({'amount'=> 2, 'balance' => product.balance})
-        expect(CartManager::Summator.serve session).to eq(product.price * 2)
+        expect(session[:cart]["#{product.id}"]).to eq({ 'amount' => 2, 'balance' => product.balance })
+        expect(CartManager::Summator.serve(session)).to eq(product.price * 2)
       end
 
       it 'add different two products and get sum' do
         post add_product_path(product)
         post plus_product_to_carts_path(id: product2.id)
-        expect(CartManager::Summator.serve session).to eq(product.price + product2.price)
+        expect(CartManager::Summator.serve(session)).to eq(product.price + product2.price)
       end
 
       it 'add different two products and minus last and get sum' do
@@ -34,7 +34,7 @@ RSpec.describe "Carts", type: :request do
         expect(response).to have_http_status(:redirect)
         post minus_product_to_carts_path(id: product2.id)
         expect(response).to have_http_status(:redirect)
-        expect(CartManager::Summator.serve session).to eq(product.price)
+        expect(CartManager::Summator.serve(session)).to eq(product.price)
       end
     end
   end
